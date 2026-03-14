ea6c587e44fd22e2caf01fdb34400c3371972f16
sgorg2011-png <sgorg2011@gmail.com>
2026-03-15 00:01:41 +0200

    Minor updates before merge

--- lib/features/auth/data/auth_service.dart
+++ lib/features/auth/data/auth_service.dart
@@ -5,7 +5,7 @@ import 'package:google_sign_in/google_sign_in.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  // Get current user
+  
   User? get currentUser => _auth.currentUser;
 
   // Stream of auth state changes
